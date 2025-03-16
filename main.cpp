7f6295d044e9be8b28a1451684b3a2ccddea281a
MonilRathod <146113573+MonilRathod@users.noreply.github.com>
2025-03-17 01:01:10 +0530

    mad a squre we are on baby

--- main.cpp
+++ main.cpp
@@ -2,14 +2,23 @@
 
 int main()
 {
-   InitWindow(800, 600, "TETRIS");
-   SetTargetFPS(60);
-   while(WindowShouldClose() == false)
-   {
-      BeginDrawing();
-      ClearBackground(RAYWHITE);
-      EndDrawing();
-   }
-   CloseWindow();
-    
+    InitWindow(800, 600, "TETRIS");
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+
+        // Draw a rectangle
+        DrawRectangle(200, 200, 100, 100, RED);
+
+        // Draw some text
+        DrawText("Hello, Tetris!", 300, 50, 20, DARKBLUE);
+
+        EndDrawing();
+    }
+
+    CloseWindow();
+    return 0;
 }
