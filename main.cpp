43c44e474b219c152d8ad7c5fabec0beb4633af7
MonilRathod <146113573+MonilRathod@users.noreply.github.com>
2025-03-17 00:38:54 +0530

    sstrting to code the game

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
-#include <iostream>
 #include <raylib.h>
 
-int main(int, char**){
-    std::cout << "Hello, from tetris!\n";
+int main()
+{
+   InitWindow(800, 600, "TETRIS");
+   SetTargetFPS(60);
+   while(WindowShouldClose() == false)
+   {
+      BeginDrawing();
+      ClearBackground(RAYWHITE);
+      EndDrawing();
+   }
+   CloseWindow();
+    
 }
