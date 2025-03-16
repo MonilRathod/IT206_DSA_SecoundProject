bbae361955a9ebc2adc2807464fa129c94fcdca3
MonilRathod <146113573+MonilRathod@users.noreply.github.com>
2025-03-16 19:24:47 +0530

    its giving me hell

--- Raylib-CPP-Starter-Template-for-VSCODE-main/main.cpp
+++ Raylib-CPP-Starter-Template-for-VSCODE-main/main.cpp
@@ -1,40 +1,17 @@
-#include <iostream>
+#include <bits/stdc++.h>
 #include <raylib.h>
 using namespace std;
 
 int main () {
+    // Initialization
+    const int screenWidth = 800;
+    const int screenHeight = 450;
+    InitWindow(screenWidth, screenHeight, "Raylib Template");
 
-    const int SCREEN_WIDTH = 800;
-    const int SCREEN_HEIGHT = 600;
-    int ball_x = 100;
-    int ball_y = 100;
-    int ball_speed_x = 5;
-    int ball_speed_y = 5;
-    int ball_radius = 15;
-    
-    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "My first RAYLIB program!");
-    SetTargetFPS(60);
+    SetTargetFPS(60); // Set our game to run at 60 frames-per-second
 
-    while (WindowShouldClose() == false){
-   
-        ball_x += ball_speed_x;
-        ball_y += ball_speed_y;
+    // Main game loop
+    while (!WindowShouldClose()) { // Detect window close button or ESC key
+        // Update
 
-        if(ball_x + ball_radius >= SCREEN_WIDTH || ball_x - ball_radius <= 0)
-        {
-            ball_speed_x *= -1;
-        }
-
-        if(ball_y + ball_radius >= SCREEN_HEIGHT || ball_y - ball_radius <= 0)
-        {
-            ball_speed_y *= -1;
-        }
-        
-        BeginDrawing();
-            ClearBackground(BLACK);
-            DrawCircle(ball_x,ball_y,ball_radius, WHITE);
-        EndDrawing();
-    }
-
-    CloseWindow();
 }
